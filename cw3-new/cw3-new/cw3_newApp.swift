d4f2fc83b8d5cfbaaea98f3324a5bbadb260cc80
hooreya39 <109871728+hooreya39@users.noreply.github.com>
2022-08-03 20:23:49 +0300

    .

---
+++ cw3-new/cw3-new/cw3_newApp.swift
@@ -0,0 +1,17 @@
+//
+//  cw3_newApp.swift
+//  cw3-new
+//
+//  Created by Hoor on 03/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct cw3_newApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
